99482d874c9352900f486eb7c609150dcc89768d
Filippo <116024248+handlePippo@users.noreply.github.com>
2023-04-02 15:16:09 +0200

    Add files via upload

---
+++ src/components/ProjectsSection.js
@@ -0,0 +1,43 @@
+import React from "react";
+import FullScreenSection from "./FullScreenSection";
+import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
+import Card from "./Card";
+
+const projects = [
+  {
+    title: "Skigames",
+    description:
+      "Prototipo di e-Commerce di videogiochi, realizzato come progetto finale del Bootcamp con Aulab",
+    getImageSrc: () => require("../images/skig.png"),
+  },
+];
+
+const ProjectsSection = () => {
+  return (
+    <FullScreenSection
+      backgroundColor='#14532d'
+      isDarkBackground
+      p={8}
+      alignItems='flex-start'
+      spacing={8}
+    >
+      <Heading as='h1' id='projects-section'>
+        Progetti
+      </Heading>
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={8} mt={8}>
+        {projects.map((project) => (
+          <Box key={project.title}>
+            <Card
+              title={project.title}
+              description={project.description}
+              imageSrc={project.getImageSrc()}
+            />
+            <p style={{ fontSize: "50px" }}>Sezione in costruzione..</p>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </FullScreenSection>
+  );
+};
+
+export default ProjectsSection;
